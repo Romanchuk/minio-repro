97131d37c56fc99a9a60852950a1bec0fc2bfb1f
Sergey Romanchuk <rezety@gmail.com>
2022-12-22 20:40:52 +0700

    MINIO_CONNECTION

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { MINIO_CONNECTION } from 'nestjs-minio';
+import { Client } from 'minio'
 
 @Injectable()
 export class AppService {
+
+  constructor(@Inject(MINIO_CONNECTION) private readonly client: Client) {}
+  
   getHello(): string {
     return 'Hello World!';
   }
